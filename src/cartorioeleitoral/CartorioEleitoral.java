27702c413d52f0d28606374bd29e33477adf0ffe
André Gomes <33938137+AndreGomesS@users.noreply.github.com>
2017-11-23 16:10:07 -0200

    Versão 1.1

--- src/cartorioeleitoral/CartorioEleitoral.java
+++ src/cartorioeleitoral/CartorioEleitoral.java
@@ -9,6 +9,7 @@ public class CartorioEleitoral {
     public static void main(String[] args) {
         System.out.println("oi ");
         System.out.println("segundo oi ");
+        System.out.println("Terceiro oi ");
     }
 
 }
