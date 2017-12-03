0614e268f2c8371b47d7d5a2b90f2e32388e4829
André Gomes <33938137+AndreGomesS@users.noreply.github.com>
2017-12-03 10:57:45 -0200

    Teste envio

--- src/cartorioeleitoral/CartorioEleitoral.java
+++ src/cartorioeleitoral/CartorioEleitoral.java
@@ -21,17 +21,17 @@ public class CartorioEleitoral {
         List<Candidato> listaCandidatos = new ArrayList<>();
         List<Eleitor> listaEleitores = new ArrayList<>();
 
-<<<<<<< HEAD
-//CadastroEleitor.cadastrarEleitor("AndréEleitor", 160300003, 1990, listaEleitores);
+
+        //CadastroEleitor.cadastrarEleitor("AndréEleitor", 160300003, 1990, listaEleitores);
         //CadastroEleitor.cadastrarEleitor("AndréEleitorDois", 160300003, 1990, listaEleitores);
         CadastroCandidato.cadastrarCandidato(0, "sem foto", "André", 160300003, 1990, listaCandidatos);
         CadastroCandidato.cadastrarCandidato(0, "sem foto", "AndréDois", 160300003, 1990, listaCandidatos);
-=======
+
         //CadastroEleitor.cadastrarEleitor("AndréEleitor", 160300003, 1990, listaEleitores);
         //CadastroEleitor.cadastrarEleitor("AndréEleitorDois", 160300003, 1990, listaEleitores);
         //CadastroCandidato.cadastrarCandidato(0, "sem foto", "André", 160300003, 1990, listaCandidatos);
         //CadastroCandidato.cadastrarCandidato(0, "sem foto", "AndréDois", 160300003, 1990, listaCandidatos);
->>>>>>> 2700f13115ea762257b200a242da96b97fa16f21
+
 
         Manipulador.lerArquivoEleitor(listaEleitores);
         Manipulador.lerArquivoCandidato(listaCandidatos);
--- src/controller/CadastroCandidato.java
+++ src/controller/CadastroCandidato.java
@@ -2,10 +2,7 @@
 package controller;
 
 import IO.Manipulador;
-<<<<<<< HEAD
 import java.util.ArrayList;
-=======
->>>>>>> 2700f13115ea762257b200a242da96b97fa16f21
 import java.util.List;
 import model.Candidato;
 
@@ -17,12 +14,9 @@ import model.Candidato;
 public class CadastroCandidato {
     
     public static void cadastrarCandidato(int QtdVotos, String pathFoto, String nome, int matricula, int anoNascimento,List listaCandidatos){
-<<<<<<< HEAD
+
         Candidato candidato = new Candidato(QtdVotos, pathFoto, nome, matricula, anoNascimento);
-       // listaCandidatos.add(candidato);   
-=======
-        Candidato candidato = new Candidato(QtdVotos, pathFoto, nome, matricula, anoNascimento);             
->>>>>>> 2700f13115ea762257b200a242da96b97fa16f21
+       // listaCandidatos.add(candidato);             
         Manipulador.escreverCandidato(candidato);
     }
 
